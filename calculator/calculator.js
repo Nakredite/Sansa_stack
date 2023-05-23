02fe8bd03269706464e3e95dfce30614718794b2
Nakredite <jamesnakre@gmail.com>
2023-05-23 14:02:18 +0100

    episode one.NOde

---
+++ calculator/calculator.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/", function (req, res) {
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2;
+
+  res.send("The result of the calculation is " + result);
+});
+
+app.post("/bmicalculator", function (req, res) {
+  var wei = parseFloat(req.body.wei);
+  var hei = parseFloat(req.body.hei);
+
+  var bmi = hei / (hei * hei);
+
+  res.send("Your BMI is " + bmi);
+});
+app.listen("3000", function () {
+  console.log("be patience bro, you will get there.");
+});
